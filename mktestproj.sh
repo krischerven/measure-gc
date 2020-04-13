05d0754acffeb56d192a436c7089f942b074f390
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-04-12 21:35:57 -0400

    Update mktestproj.sh

--- mktestproj.sh
+++ mktestproj.sh
@@ -1,4 +1,4 @@
 mkdir -p testproj/
 cp main.go testproj/
 sed -i 's/package measuregc/package main/g' testproj/main.go
-echo -e '\nfunc main() {\n\tStartMeasuringGCs()\n\tselect {}\n}' >> testproj/main.go
+echo -e '\nfunc main() {\n\tStart()\n\tselect {}\n}' >> testproj/main.go
